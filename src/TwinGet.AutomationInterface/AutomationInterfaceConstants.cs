8136db360f39fc5ea7118ec48cd978c0f059d313
Giang Nguyen <nhgiangvt117@gmail.com>
2023-11-30 13:16:51 +0200

    chore: reorder content of `SupportedProgIds` List
    Putting TcXaeShell on top of the list so that `AutomationInterface` prioritize using TwinCAT XAE (over Visual Studio) when creating a DTE instance

--- src/TwinGet.AutomationInterface/AutomationInterfaceConstants.cs
+++ src/TwinGet.AutomationInterface/AutomationInterfaceConstants.cs
@@ -9,10 +9,10 @@ namespace TwinGet.AutomationInterface
     public static class AutomationInterfaceConstants
     {
         public static readonly IReadOnlyList<string> SupportedProgIds = new List<string> {
+            "TcXaeShell.DTE.15.0",
             "VisualStudio.DTE.12.0",
             "VisualStudio.DTE.14.0",
-            "VisualStudio.DTE.15.0",
-            "TcXaeShell.DTE.15.0"
+            "VisualStudio.DTE.15.0"
         };
 
     }
