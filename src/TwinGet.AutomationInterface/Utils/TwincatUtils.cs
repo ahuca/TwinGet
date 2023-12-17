999defadfab5c345150fbb82ecfee4352af08445
Giang Nguyen <nhgiangvt117@gmail.com>
2023-12-17 02:12:04 +0200

    feat: Added `GetParentTwincatProject`

--- src/TwinGet.AutomationInterface/Utils/TwincatUtils.cs
+++ src/TwinGet.AutomationInterface/Utils/TwincatUtils.cs
@@ -1,7 +1,9 @@
 ﻿// This file is licensed to you under MIT license.
 
+using System.Xml.Serialization;
 using TCatSysManagerLib;
 using TwinGet.AutomationInterface.Exceptions;
+using TwinGet.AutomationInterface.ProjectFileDeserialization;
 using ITcSmTreeItemAlias = TCatSysManagerLib.ITcSmTreeItem9;
 using ITcSysManagerAlias = TCatSysManagerLib.ITcSysManager15;
 
@@ -17,6 +19,77 @@ namespace TwinGet.AutomationInterface.Utils
             var plcProject = (ITcPlcIECProject3)treeItem ?? throw new CouldNotLookUpPlcProject($"Could not look up any PLC project with the provided name", name);
 
             return plcProject;
+        }
+
+        /// <summary>
+        /// Try to find the TwinCAT project that the given PLC project belong to.
+        /// </summary>
+        /// <param name="plcProjectPath">The path to the <c>.plcproj</c> file.</param>
+        /// <param name="upwardDepth"></param>
+        /// <returns></returns>
+        /// <exception cref="FileNotFoundException"></exception>
+        public static string GetParentTwincatProject(string plcProjectPath, int upwardDepth = 5)
+        {
+            ArgumentException.ThrowIfNullOrEmpty(plcProjectPath, nameof(plcProjectPath));
+
+            if (!File.Exists(plcProjectPath))
+            {
+                throw new FileNotFoundException($"Provided plc project file path does not exists.", plcProjectPath);
+            }
+
+            // We first try to parse the plcproj file and throw if necessary.
+            string xmlContent = File.ReadAllText(plcProjectPath);
+            XmlSerializer serializer = new(typeof(PlcProjectData));
+
+            PlcProjectData plcProjectFile;
+            using (StringReader reader = new(xmlContent))
+            {
+                plcProjectFile = serializer.Deserialize(reader) as PlcProjectData ?? throw new InvalidProjectFileFormat("Could not deserialize the provided PLC project file.", plcProjectPath);
+            }
+
+            /// We recursively look up TwinCAT project files in parent folders with a depth of <param name="upwardDepth"/param>.
+            string parent = plcProjectPath;
+            for (int i = 0; i < upwardDepth - 1; i++)
+            {
+                parent = Directory.GetParent(parent)?.FullName ?? string.Empty;
+
+                if (string.IsNullOrEmpty(parent))
+                {
+                    break;
+                }
+
+                string[] xaeCandidates = Directory.GetFiles(parent, $"*{TwincatConstants.TwincatXaeProjectExtension}");
+                string[] plcCandidates = Directory.GetFiles(parent, $"*{TwincatConstants.TwincatPlcProjectExtension}");
+                string[] twincatProjectCandidate = xaeCandidates.Concat(plcCandidates).ToArray();
+
+                if (twincatProjectCandidate.Length == 0) { continue; }
+
+                // We process each TwinCAT project file we found.
+                foreach (string tcCandidate in twincatProjectCandidate)
+                {
+                    xmlContent = File.ReadAllText(tcCandidate);
+                    serializer = new(typeof(TcSmProjectData));
+
+                    using (StringReader reader = new(xmlContent))
+                    {
+                        TcSmProjectData? tcSmProject = serializer.Deserialize(reader) as TcSmProjectData;
+
+                        if (tcSmProject is null) { continue; }
+
+                        // We process each PLC project the TwinCAT project has.
+                        foreach (ProjectElement plcProjectCandidate in tcSmProject.Project.Plc.Projects)
+                        {
+                            // Using GUID, if we find ourselves in the TwinCAT project candidate, we return the candidate.
+                            if (plcProjectCandidate.GUID.Equals(plcProjectFile.PropertyGroup.ProjectGuid, StringComparison.OrdinalIgnoreCase))
+                            {
+                                return tcCandidate;
+                            }
+                        }
+                    }
+                }
+            }
+
+            return "";
         }
     }
 }
---
+++ test/TwinGet.AutomationInterface.Test/Utils/TwincatUtilsTests.cs
@@ -0,0 +1,36 @@
+﻿// This file is licensed to you under MIT license.
+
+using TwinGet.AutomationInterface.Test.TestUtils;
+using TwinGet.AutomationInterface.Utils;
+
+namespace TwinGet.AutomationInterface.Test.Utils
+{
+    public class TwincatUtilsTests
+    {
+        [Fact]
+        public void GetParentTwincatProject_ShouldSucceed()
+        {
+            using TestProject testProject = new TestProject();
+            TestTwincatProject? testTcProject = null;
+            TestPlcProject? testPlcProject = null;
+
+            // Find a test TwinCAT project that has at least one PLC project.
+            foreach (TestTwincatProject tcProj in testProject.TwincatProjects)
+            {
+                if (tcProj.PlcProjects.Count > 0)
+                {
+                    testTcProject = tcProj;
+                    testPlcProject = tcProj.PlcProjects.First();
+                }
+            }
+
+            testTcProject.Should().NotBeNull();
+
+            string actual = TwincatUtils.GetParentTwincatProject(testPlcProject.AbsolutePath);
+            string expected = testTcProject.AbsolutePath;
+
+            expected.Should().NotBeNullOrEmpty();
+            expected.Should().Be(actual);
+        }
+    }
+}
