90bf6fa3f6aa09436478e3da70e03a1bd5d862c1
Giang Nguyen <nhgiangvt117@gmail.com>
2024-02-17 21:49:26 +0200

    refactor: Added settings for DTE instance on creation

--- src/TwinGet.TwincatInterface/TwincatDteProvider.cs
+++ src/TwinGet.TwincatInterface/TwincatDteProvider.cs
@@ -65,6 +65,10 @@ public class TwincatDteProvider : IDisposable
             );
         }
 
+        Dte.SuppressUI = true;
+        Dte.MainWindow.Visible = false;
+        Dte.UserControl = false;
+
         if (startMsgFiltering)
         {
             _startedMsgFiltering = true;
